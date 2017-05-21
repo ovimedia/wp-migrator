fc705afdf36c279129fa7202fde611f54e02de18
ovimedia <info@ovimedia.es>
2017-05-21 02:16:35 +0200

    1.0

--- wp_migrator.php
+++ wp_migrator.php
@@ -1,18 +1,34 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+if(isset($_REQUEST["old_url"]))
+{
+    require_once( './wp-load.php' );
+    require_once('./wp-includes/option.php');
 
-require_once( './wp-load.php' );
+    global $wpdb;
 
-global $wpdb;
+    $old_url = $_REQUEST["old_url"]; 
+    $new_url = $_REQUEST["new_url"];  
 
-$old_url = $_REQUEST["old_url"]; 
-$new_url = $_REQUEST["new_url"];  
+    $wpdb->query("update $wpdb->posts set guid = replace(guid, '$old_url', '$new_url'), post_content = replace(post_content, '$old_url', $new_url')");
 
-$wpdb->query("update $wpdb->posts set guid = replace(guid, '$old_url', '$new_url'), post_content = replace(post_content, '$old_url', $new_url')");
+    $wpdb->query("update $wpdb->postmeta set meta_value = replace(meta_value, '$old_url', '$new_url')");
 
-$wpdb->query("update $wpdb->postmeta set meta_value = replace(meta_value, '$old_url', '$new_url')");
+    $wpdb->query("update $wpdb->options set option_value = '$new_url' where option_value like '$old_url'");
+    
+    $results = $wpdb->get_results("select option_name from $wpdb->options where option_value like '%$old_url%'");
+    
+    foreach($results as $option)
+    {        
+        $value = get_option($option->option_name);
 
-$wpdb->query("update $wpdb->options set option_value = '$new_url' where option_value like '$old_url'");
-
+        $value = json_decode(str_replace($old_url, $new_url, json_encode($value)));
+        
+        update_option($option->option_name, $value);
+    }
+}
 ?>
 
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" >
