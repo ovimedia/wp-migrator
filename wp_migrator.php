44d61d03744026b9a77d8663f72c37f98706630e
ovimedia <info@ovimedia.es>
2017-05-22 21:46:08 +0200

    1.0

--- wp_migrator.php
+++ wp_migrator.php
@@ -9,9 +9,12 @@ if(isset($_REQUEST["old_url"]))
     $old_url = $_REQUEST["old_url"]; 
     $new_url = $_REQUEST["new_url"];  
 
-    $wpdb->query("update $wpdb->posts set guid = replace(guid, '$old_url', '$new_url'), post_content = replace(post_content, '$old_url', $new_url')");
+    $wpdb->query("update $wpdb->posts set guid = replace(guid, '$old_url', '$new_url'), 
+    post_content = replace(post_content, '$old_url', '$new_url')");
 
     $wpdb->query("update $wpdb->postmeta set meta_value = replace(meta_value, '$old_url', '$new_url')");
+    
+    $wpdb->query("update $wpdb->options set option_value = '$new_url' where option_name like '$old_url'");
 
     $results = $wpdb->get_results("select option_name from $wpdb->options where option_value like '%$old_url%'");
     
@@ -31,8 +34,8 @@ if(isset($_REQUEST["old_url"]))
 
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" >
     
-    <p><input type="text" placeholder="Old URL" name="old_url" id="old_url" style="min-width: 500px" /></p>
-    <p><input type="text" placeholder="New URL" name="new_url" id="new_url" style="min-width: 500px"  /></p>
+    <p><input type="text" placeholder="Antigua URL" name="old_url" id="old_url" style="min-width: 500px" /></p>
+    <p><input type="text" placeholder="Nueva URL" name="new_url" id="new_url" style="min-width: 500px"  /></p>
     <p><input type="submit" value="Reemplazar URL´s" /></p>
     
 </form>
